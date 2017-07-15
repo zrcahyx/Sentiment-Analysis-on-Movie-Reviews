f23003dfb224d7c2de85071999c1c5ed21e550b7
zrcahyx <ruchenzhang93@gmail.com>
2017-07-15 10:56:18 +0800

    add num 00V

--- scripts/data/gen_oov_set.py
+++ scripts/data/gen_oov_set.py
@@ -20,13 +20,18 @@ def main():
     with open(file_path, 'r') as f:
         word_set = pickle.load(f)
 
-    oov_set = set()
+    oov_set, num_oov = set(), 0
+    for v in list(word_set):
+        if not v in word2Idx.keys():
+            num_oov += 1
+            oov_set.add(v)
     pickle.dump(oov_set,
                 open(join(pickle_data_dir, sys.argv[1] + '_oov_set.dat'), "wb"),
                 True)
 
     print('OOV set for ' + sys.argv[1] + ' dataset is:')
     print(oov_set)
+    print('Number of OOV word in ' + sys.argv[1] + ' dataset is: {}'.format(num_oov))
 
 if __name__ == '__main__':
     main()
