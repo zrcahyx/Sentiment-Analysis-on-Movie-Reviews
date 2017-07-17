d29014c803cbdf15042ba5a2d72853234348b33d
zrcahyx <ruchenzhang93@gmail.com>
2017-07-17 09:10:39 +0800

    batch_size = FLAGS.batch_size

--- scripts/train/train_normal.py
+++ scripts/train/train_normal.py
@@ -80,8 +80,9 @@ class Input(object):
         elif mode == 'train':
             input, label, _, _ = read_and_decode(filename_queue, mode)
             label = tf.one_hot(label, FLAGS.output_units, name='label')
+            batch_size = FLAGS.batch_size
             input, label = tf.train.shuffle_batch([input, label],
-                                                  batch_size=FLAGS.batch_size,
+                                                  batch_size=batch_size,
                                                   num_threads=2,
                                                   capacity=1000 + 3 * batch_size,
                                                   min_after_dequeue=1000)
